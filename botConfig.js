88d9fbae4455fe35940e37a24b0369566ec7606a
Tariel <tarieltairov1@gmail.com>
2024-04-10 12:04:05 +0300

    minor

--- botConfig.js
+++ botConfig.js
@@ -2,7 +2,7 @@ const TelegramApi = require("node-telegram-bot-api");
 const { commandsBtns } = require("./common/constants/commands");
 require("dotenv").config();
 
-const token = process.env.BOT_TOKEN;
+const token = process.env.BOT_TOKENN;
 const replenishmentGroupId = process.env.REPLENISHMENT_GROUP_ID;
 const conclusionGroupId = process.env.CONCLUSION_GROUP_ID;
 
